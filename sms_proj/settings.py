421564ece4d0658cdfea1b30e39f767cfc8d3d05
Clement Danso <clement.quoda@gmail.com>
2021-01-23 23:14:20 +0000

    Changes to be committed:

--- sms_proj/settings.py
+++ sms_proj/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b%60h&r9-3$g2dztlox@k=%d2mswf@b1^^tz$48ackgi*c(4!o'
+#SECRET_KEY = 'b%60h&r9-3$g2dztlox@k=%d2mswf@b1^^tz$48ackgi*c(4!o'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rhdsms.herokuapp.com']
 
 
 # Application definition
